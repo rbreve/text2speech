16f8737282e8f3cab5776d8b862522b2047b1d4d
Roberto Brevė <roberto.breve@anders.com>
2024-11-15 01:54:48 +0200

    Initial Commit
    First commit with django initial project files.
    Added User model.
    Added django rest framework.
    Added basic user api authentication
    Added necessary models for initial project

---
+++ texttospeech/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TexttospeechConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "texttospeech"
---
+++ users/views.py
@@ -0,0 +1,19 @@
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from .serializers import RegisterSerializer, UserSerializer
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
+
+class UserDetailView(generics.RetrieveUpdateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
