5e540d8829a9ba01a28907d246e617100e5df010
Roberto Brevė <roberto.breve@anders.com>
2024-11-21 01:51:40 +0200

    Added simple test for TTS Request

---
+++ texttospeech/tests/test_api.py
@@ -0,0 +1,20 @@
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from texttospeech.models import TTSConverter
+from users.views import User
+
+class TTSRequestTests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="testi", password="testi")
+        self.client.force_authenticate(user=self.user)
+
+    def test_create_tts_request(self):
+        url = reverse("create-tts-request")
+        data = {"text": "Convert this text to speech"}
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(TTSConverter.objects.count(), 1)
+        self.assertEqual(response.data["status"], "Pending")
+        self.assertNotEqual(response.data["id"], None)
+        self.assertNotEqual(response.data["request_id"], "")
--- texttospeech/urls.py
+++ texttospeech/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import CreateTTSRequestView, TTSUserTextsView
 
 urlpatterns = [
-    path("request/", CreateTTSRequestView.as_view(), name="tts-request"),
-    path("texts/", TTSUserTextsView.as_view(), name="tts-texts"),
+    path("request/", CreateTTSRequestView.as_view(), name="create-tts-request"),
+    path("texts/", TTSUserTextsView.as_view(), name="list-tts-texts"),
 ]
